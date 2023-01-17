2bf5a649ce16023c9822d7b66d9d498891e7ba4e
Jussi Ccoyllo <jussi.ccoyllo@gmail.com>
2023-01-17 14:53:20 +0100

    Update 2

--- DataMoviesQueries.sql
+++ DataMoviesQueries.sql
@@ -47,7 +47,10 @@ JOIN genre g on g.genre_id = mg.genre_id
 WHERE genre_name LIKE 'Horror';
 
 -- 9
-
+SELECT * FROM movie
+JOIN movie_genres mg on movie.movie_id = mg.movie_id
+JOIN genre g on mg.genre_id = g.genre_id
+WHERE genre_name LIKE '';
 
 -- Tabel: person, movie_cast
 -- 10
@@ -58,10 +61,47 @@ JOIN person p on movie_cast.person_id = p.person_id;
 
 -- Tabel Movie
 -- 11
-SELECT title, AVG(runtime)
-FROM movie
-GROUP BY title
-HAVING  AVG(runtime) > 106.000;
+SELECT * FROM movie
+WHERE runtime > (SELECT AVG(runtime) FROM movie)
+ORDER BY title;
+
+CREATE TABLE message (
+msg_id INT AUTO_INCREMENT,
+message VARCHAR(255),
+user_id INT NOT NULL,
+receiver_id INT NOT NULL,PRIMARY KEY (msg_id)
+);
+CREATE TABLE usr (
+usr_id INT,
+login VARCHAR(50),
+email VARCHAR(255)
+);
+
+-- 12
+INSERT INTO usr (usr_id, login, email)
+VALUES (1, 'rincewind',  'rincewind@discworld.org');
+
+-- 13
+INSERT INTO usr (usr_id, login, email)
+VALUES (2, 'Bursar',  'bursar@unseen.university'),
+       (3, 'archchancellor', 'arch@unseen.university');
+
+-- 14
+INSERT INTO message (message, user_id, receiver_id)
+VALUES ('Bursar?', 3, 2),
+       ('Yes, Archchancellor?', 2, 3),
+       ('You aren''t a member of some secret society or somethin'', are you?', 3, 2),
+       ('Me? No, Archchancellor', 2, 3),
+       ('Thenit''d be a damn good idea to take your underpants off your head', 3, 2);
+
+-- 15
+UPDATE usr
+SET email = 'rincewind@unseen.university'
+WHERE email = 'rincewind@discworld.org';
+
+-- 16
+DELETE FROM message
+WHERE user_id = 2
 
 
 
